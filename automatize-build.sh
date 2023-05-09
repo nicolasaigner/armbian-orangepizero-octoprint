e66603cc050a19991b2d1d684fddac9ccf55be9e
Nícolas Aigner <nicolas.aigner@gmail.com>
2023-05-09 19:52:31 -0300

    Atualização dos parâmetros utilizados

--- automatize-build.sh
+++ automatize-build.sh
@@ -13,7 +13,7 @@ cp armbian/customize-image.sh build/userpatches
 cd build
 
 # Execute o script compile.sh com as configurações desejadas
-./compile.sh BOARD=orangepizero BRANCH=current BUILD_DESKTOP=no BUILD_MINIMAL=yes KERNEL_CONFIGURE=no RELEASE=focal SHARE_LOG=yes | tee build.log
+./compile.sh BOARD=orangepizero BRANCH=current BUILD_DESKTOP=no BUILD_MINIMAL=yes KERNEL_CONFIGURE=no RELEASE=focal DEST_LANG="pt_BR.UTF-8" SHARE_LOG=yes | tee build.log
 
 # Quando o build estiver completo, encontre a URL do log e abra no navegador default
 url=$(grep -o 'https://paste.next.armbian.com/.*' build.log)
