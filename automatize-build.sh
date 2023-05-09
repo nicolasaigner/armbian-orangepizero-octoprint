869b624c219a47bf71e88d9840e91fd4fdfd66ef
Nícolas Aigner <nicolas.aigner@gmail.com>
2023-05-09 19:43:33 -0300

    Update automatize-build.sh
    Atualização do código do script

--- automatize-build.sh
+++ automatize-build.sh
@@ -13,7 +13,7 @@ cp customize-image.sh build/userpatches
 cd build
 
 # Execute o script compile.sh com as configurações desejadas
-BOARD=orangepizero BRANCH=current BUILD_DESKTOP=no BUILD_MINIMAL=yes KERNEL_CONFIGURE=no RELEASE=focal SHARE_LOG=yes ./compile.sh | tee build.log
+./compile.sh BOARD=orangepizero BRANCH=current BUILD_DESKTOP=no BUILD_MINIMAL=yes KERNEL_CONFIGURE=no RELEASE=focal SHARE_LOG=yes | tee build.log
 
 # Quando o build estiver completo, encontre a URL do log e abra no navegador default
 url=$(grep -o 'https://paste.next.armbian.com/.*' build.log)
